0d8dfd2735e5af0f4ef750c3fa7cae6782281921
iDatLe <LeDatDac@gmail.com>
2019-06-24 16:10:43 -0700

    Successfully implemented NetlifyCMS with gatsby and able to create pages from new blog markdown files

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,24 +1,45 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 export default function Template() {
   const data = useStaticQuery(
     graphql`
       query {
-        markdownRemark {
-          html
-          frontmatter {
-            title
-            path
-            date(formatString: "MMMM DD, YYYY")
+        allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC }) {
+          edges {
+            node {
+              id
+              html
+              frontmatter {
+                title
+                path
+                date(formatString: "MMMM DD YYYY")
+              }
+              fields {
+                slug
+              }
+              excerpt
+            }
           }
-        } 
+        }
       }
+      
     `
   )
   console.log(data)
+
+
   return (
-    <>
-    </>
+    <div className="container">
+      {data.allMarkdownRemark.edges.map(({node}) => (
+        <div key={node.id}>
+          <Link to={node.fields.slug}>
+            <h3>{node.frontmatter.tittle}{" "}</h3>
+            <p>{node.excerpt}</p>
+          </Link>
+        </div>
+      ))}
+
+    </div>
   )
 }
