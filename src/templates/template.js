25fb774c0e0077ae15ac4fbc33a5d6442eade66e
iDatLe <LeDatDac@gmail.com>
2019-06-25 16:00:39 -0700

    Changed authentication from github to netlify

--- src/templates/template.js
+++ src/templates/template.js
@@ -5,6 +5,7 @@ export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <>
+      <h1> Welcome to a blog post </h1>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{__html: post.html}} />
     </>
